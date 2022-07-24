a0382bc3db9ba158dc272a1e077b45b38d541091
Henna Kalliokoski <henna.kalliokoski@reaktor.com>
2022-07-25 01:04:24 +0300

    fix env

--- src/pages/api/location.js
+++ src/pages/api/location.js
@@ -2,7 +2,7 @@ import { Octokit } from "@octokit/core";
 
 const updateGist = (con) => {
   const octokit = new Octokit({
-    auth: GIST_TOKEN,
+    auth: process.env.GIST_TOKEN,
   });
 
   octokit.request("PATCH /gists/f097ca1b5c5b8d8f7d76720c76fbf061", {
@@ -18,6 +18,7 @@ const updateGist = (con) => {
 
 export default async function handler(req, res) {
   const { body, XTelegramBotApiSecretToken } = req;
-  if (XTelegramBotApiSecretToken === BOT_WEBHOOK_TOKEN) updateGist(body);
+  if (XTelegramBotApiSecretToken === process.env.BOT_WEBHOOK_TOKEN)
+    updateGist(body);
   return res.send("ok");
 }

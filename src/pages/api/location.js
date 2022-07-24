302a3c1957ad5a6f19ef0f5586221ca7c63e66f1
Henna Kalliokoski <henna.kalliokoski@reaktor.com>
2022-07-25 01:36:06 +0300

    testing

--- src/pages/api/location.js
+++ src/pages/api/location.js
@@ -19,6 +19,6 @@ const updateGist = (con) => {
 export default async function handler(req, res) {
   const { body, query } = req;
   if (query.secret_token === process.env.BOT_WEBHOOK_TOKEN)
-    updateGist(body.update.message.location);
+    updateGist(body.update);
   return res.send("ok");
 }

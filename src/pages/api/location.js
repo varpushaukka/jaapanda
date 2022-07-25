e0f8c1202a05deda6fe9ed8ddb2ee5e54d45990d
Henna Kalliokoski <henna.kalliokoski@reaktor.com>
2022-07-25 22:17:22 +0300

    handle message

--- src/pages/api/location.js
+++ src/pages/api/location.js
@@ -3,14 +3,21 @@ import { Octokit } from "@octokit/core";
 process.env.NTBA_FIX_319 = "test";
 const TelegramBot = require("node-telegram-bot-api");
 
-const updateGist = (con) => {
+const updateGist = (isOpen, location = [60.23165, 25.03632], token) => {
+  const content = { open: isOpen, lat: location[0], lon: location[1] };
+  if (token === process.env.BOT_WEBHOOK_TOKEN) {
+    updateGistCall(JSON.stringify(content));
+  }
+};
+
+const updateGistCall = (con) => {
   const octokit = new Octokit({
     auth: process.env.GIST_TOKEN,
   });
 
   octokit.request("PATCH /gists/f097ca1b5c5b8d8f7d76720c76fbf061", {
     gist_id: "f097ca1b5c5b8d8f7d76720c76fbf061",
-    description: "An update to a gist",
+    description: "Bot update to status",
     files: {
       "jaapanda.json": {
         content: con,
@@ -23,20 +30,27 @@ export default async function handler(req, res) {
   try {
     const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
     const { body, query } = req;
-    if (body.message) {
+    if (body.message.text.toLowerCase() == "kiinni") {
       const {
         chat: { id },
-        text,
       } = body.message;
-
-      const message = `Thanks for your message: *"${text}"*\nHave a great day!;`;
-
+      //updateGist(false, query.secret_token)
+      const message = `[Jääpanda](https://xn--jpanda-buaa.fi/) on nyt kiinni. Lähetä sijainti avataksesi sen uudelleen.`;
+      await bot.sendMessage(id, message, { parse_mode: "Markdown" });
+    } else if (body.message.location) {
+      const {
+        chat: { id },
+        location,
+      } = body.message;
+      const message = `sijaintisi on *"${
+        (location.longitude, location.latitude)
+      }"*Jääpanda on nyt auki!`;
+      //updateGist(true, [location.latitude, location.longitude], query.secret_token);
       await bot.sendMessage(id, message, { parse_mode: "Markdown" });
     }
   } catch (error) {
     console.error("Error sending message");
     console.log(error.toString());
   }
-  // if (query.secret_token === process.env.BOT_WEBHOOK_TOKEN) updateGist(body);
   return res.send("ok");
 }

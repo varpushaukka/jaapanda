b440b75b8c6069fe986db19ed8b67d7b3125c9e6
Henna Kalliokoski <henna.kalliokoski@reaktor.com>
2022-07-25 21:09:50 +0300

    use telegram bot node library

--- src/pages/api/location.js
+++ src/pages/api/location.js
@@ -1,5 +1,8 @@
 import { Octokit } from "@octokit/core";
 
+process.env.NTBA_FIX_319 = "test";
+const TelegramBot = require("node-telegram-bot-api");
+
 const updateGist = (con) => {
   const octokit = new Octokit({
     auth: process.env.GIST_TOKEN,
@@ -17,7 +20,23 @@ const updateGist = (con) => {
 };
 
 export default async function handler(req, res) {
-  const { body, query } = req;
+  try {
+    const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+    const { body, query } = req;
+    if (body.message) {
+      const {
+        chat: { id },
+        text,
+      } = body.message;
+
+      const message = `Thanks for your message: *"${text}"*\nHave a great day!;`;
+
+      await bot.sendMessage(id, message, { parse_mode: "Markdown" });
+    }
+  } catch (error) {
+    console.error("Error sending message");
+    console.log(error.toString());
+  }
   // if (query.secret_token === process.env.BOT_WEBHOOK_TOKEN) updateGist(body);
   return res.send("ok");
 }
